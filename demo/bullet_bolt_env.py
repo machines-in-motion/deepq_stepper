822f27e55b5ad7dd45c859bc98998bb9eed1aa15
avadesh02 <avadesh02@gmail.com>
2020-05-06 23:47:26 -0400

    finished setting up bolt environment

--- demo/bullet_bolt_env.py
+++ demo/bullet_bolt_env.py
@@ -4,32 +4,34 @@
 
 from py_bullet_env.bullet_bolt_env import BoltBulletEnv
 
-kp = [100, 100, 100]
-kd = [1, 1, 1]
-kp_com = [100, 100, 100]
-kd_com = [2, 2, 30]
-kp_ang_com = [100, 100, 100]
-kd_ang_com = [100, 100, 100]
+kp = [50, 50, 50]
+kd = [1, 1, 10]
+kp_com = [10, 0, 0]
+kd_com = [10, 0, 0]
+kp_ang_com = [10, 10, 10]
+kd_ang_com = [1, 1, 1]
 
 step_time = 0.1
+stance_time = 0.01
 ht = 0.2
 des_com = [0.0, 0, ht]
 des_vel = [0, 0, 0]
 x_ori = [0, 0, 1, 0]
 x_angvel = [0, 0, 0]
 
-bolt_env = BoltBulletEnv(ht, step_time, kp, kd, kp_com, kd_com, kp_ang_com, kd_ang_com)
+bolt_env = BoltBulletEnv(ht, step_time, stance_time, kp, kd, kp_com, kd_com, kp_ang_com, kd_ang_com)
 
 x, xd, u, n = bolt_env.reset_env()
 
 state = [x[0] - u[0], x[1] - u[1], xd[0], xd[1], n]
-print(state)
-print("init")
+# print(state)
+# print("init")
+# bolt_env.update_gains([0, 0, 0], kd, kp_com, kd_com, kp_ang_com, kd_ang_com)
 for i in range(50):
     if n > 0:
-        action = [0.0, 0.1]
+        action = [0.0, 0.07]
     if n < 0:
-        action = [0.0, -0.1]
+        action = [0.0, -0.07]
     x, xd, u, n = bolt_env.step_env(action, des_com, des_vel, x_ori, x_angvel)
-    print(u, n)
+    # print(u, n)
     state = [x[0] - u[0], x[1] - u[1], xd[0], xd[1], n]
--- python/py_bullet_env/bullet_bolt_env.py
+++ python/py_bullet_env/bullet_bolt_env.py
@@ -23,11 +23,12 @@ from pinocchio.utils import zero
 
 class BoltBulletEnv:
 
-    def __init__(self, ht, step_time, kp, kd, kp_com, kd_com, kp_ang_com, kd_ang_com, ifrecord = False):
+    def __init__(self, ht, step_time, stance_time, kp, kd, kp_com, kd_com, kp_ang_com, kd_ang_com, ifrecord = False):
         '''
         Input:
             ht : height of the COM above the ground
             step_time : the duration after which a step is taken
+            stance_time : duration of double support
             kp : p gain in leg impedance
             kd : d gain in leg impedance
             kp_com : p gain in wbc
@@ -38,6 +39,7 @@ class BoltBulletEnv:
 
         self.ht = ht
         self.step_time = step_time
+        self.stance_time = stance_time
         self.dt = 0.001
         
         self.robot = BoltRobot()
@@ -53,7 +55,7 @@ class BoltBulletEnv:
 
         # Centroidal controller initialisation
         self.centr_controller = BoltCentroidalController(self.robot.pin_robot, self.total_mass,
-        mu=0.5, kp=kp_com, kd=kd_com, kpa=kp_ang_com, kda=kd_ang_com,
+        mu=5, kp=kp_com, kd=kd_com, kpa=kp_ang_com, kda=kd_ang_com,
         eff_ids=self.robot.pinocchio_endeff_ids)
         self.F = np.zeros(12)
 
@@ -66,11 +68,12 @@ class BoltBulletEnv:
         self.sse = BoltStateEstimator(self.robot.pin_robot)
 
     def update_gains(self, kp, kd, kp_com, kd_com, kp_ang_com, kd_ang_com):
-        self.kp = 4 * kp
-        self.kd = 4 * kd
+        self.kp = 2 * kp
+        self.kd = 2 * kd
         self.centr_controller = BoltCentroidalController(self.robot.pin_robot, self.total_mass,
-        mu=0.5, kc=kp_com, dc=kd_com, kb=kp_ang_com, db=kd_ang_com,eff_ids=self.robot.pinocchio_endeff_ids)        
-
+        mu=0.5, kp=kp_com, kd=kd_com, kpa=kp_ang_com, kda=kd_ang_com,
+        eff_ids=self.robot.pinocchio_endeff_ids)
+        
     def reset_env(self):
         q0 = np.matrix(BoltConfig.initial_configuration).T
         dq0 = np.matrix(BoltConfig.initial_velocity).T
@@ -93,31 +96,36 @@ class BoltBulletEnv:
        
         return np.around(com[0:2].T, 2), np.around(dcom[0:2].T, 2), np.around(u[0:2], 2), np.power(-1, self.n + 1)
 
-    def generate_traj(self, q, dq, fl_foot, fr_foot, n, u_t, t):
+    def generate_traj(self, q, dq, fl_foot, fr_foot, n, u_t, t, stance_time):
         '''
         This function returs the desired location of the foot for the 
         given time step
         '''
 
-        x_des = 2*[0.0, 0.0, -self.ht]
+        x_des = 2*[0.0, 0.0, 0]
+        xd_des = 2*[0.0, 0.0, 0]
+        
         fl_hip, fr_hip = self.sse.return_hip_locations(q, dq)
+        if t < self.step_time - stance_time:
+            if np.power(-1, n) < 0: ## fr leave the ground
+                cnt_array = [1, 0]
 
-        if np.power(-1, n) < 0: ## fr leave the ground
-            cnt_array = [1, 0]
+                u_t_des = [[fl_foot[0], fl_foot[1], 0.0], [u_t[0], u_t[1], 0.0]]
+                x_des[0:3] = np.subtract(u_t_des[0], [fl_hip[0], fl_hip[1], self.ht])
+                x_des[3:6], xd_des[3:6] = self.trj.generate_foot_traj([fr_foot[0], fr_foot[1],0.0], u_t_des[1], [0.0, 0.0, self.f_lift], self.step_time - stance_time,t)
+                x_des[3:6] = np.subtract(x_des[3:6], [fr_hip[0], fr_hip[1], self.ht])
+                
+            if np.power(-1, n) > 0: # fl leaves the ground
+                cnt_array = [0, 1]
 
-            u_t_des = [[fl_foot[0], fl_foot[1], 0.0], [u_t[0], u_t[1], 0.0]]
-            x_des[0:3] = np.subtract(self.trj.generate_foot_traj([fl_foot[0], fl_foot[1],0.0], u_t_des[0], [0.0, 0.0, 0.0], self.step_time,t), [fl_hip[0], fl_hip[1], self.ht])
-            x_des[3:6] = np.subtract(self.trj.generate_foot_traj([fr_foot[0], fr_foot[1],0.0], u_t_des[1], [0.0, 0.0, self.f_lift], self.step_time,t), [fr_hip[0], fr_hip[1], self.ht])
-            
-        if np.power(-1, n) > 0: # fl leaves the ground
-            cnt_array = [0, 1]
+                u_t_des = [[u_t[0], u_t[1], 0.0], [fr_foot[0], fr_foot[1], 0.0]]
+                x_des[0:3], xd_des[0:3] = self.trj.generate_foot_traj([fl_foot[0], fl_foot[1],0.0], u_t_des[0], [0.0, 0.0, self.f_lift], self.step_time - stance_time,t)
+                x_des[0:3] = np.subtract(x_des[0:3], [fl_hip[0], fl_hip[1], self.ht])
+                x_des[3:6] = np.subtract(u_t_des[1], [fr_hip[0], fr_hip[1], self.ht])
+        else:
+            cnt_array = [1, 1]
 
-            u_t_des = [[u_t[0], u_t[1], 0.0], [fr_foot[0], fr_foot[1], 0.0]]
-            x_des[0:3] = np.subtract(self.trj.generate_foot_traj([fl_foot[0], fl_foot[1],0.0], u_t_des[0], [0.0, 0.0, self.f_lift], self.step_time,t), [fl_hip[0], fl_hip[1], self.ht])
-            x_des[3:6] = np.subtract(self.trj.generate_foot_traj([fr_foot[0], fr_foot[1],0.0], u_t_des[1], [0.0, 0.0, 0.0], self.step_time,t), [fr_hip[0], fr_hip[1], self.ht])
-            
-
-        return x_des, cnt_array
+        return x_des, xd_des, cnt_array
 
     def step_env(self, action, des_com, des_vel, x_ori, x_angvel):
         '''
@@ -138,15 +146,12 @@ class BoltBulletEnv:
             q, dq = self.robot.get_state()
             com = np.reshape(np.array(q[0:3]), (3,))
             dcom = np.reshape(np.array(dq[0:3]), (3,))
-
-            x_des, cnt_array = self.generate_traj(q, dq, fl_foot, fr_foot, self.n, u_t, self.dt*t)
-
+            x_des, xd_des, cnt_array = self.generate_traj(q, dq, fl_foot, fr_foot, self.n, u_t, self.dt*t, self.stance_time)
 
             w_com = self.centr_controller.compute_com_wrench(q, dq, des_com, des_vel, x_ori, x_angvel)
             w_com[2] += self.total_mass * 9.81
             F = self.centr_controller.compute_force_qp(q, dq, cnt_array, w_com)
-
-            tau = self.bolt_leg_ctrl.return_joint_torques(q,dq,self.kp,self.kd,x_des, self.xd_des,F)
+            tau = self.bolt_leg_ctrl.return_joint_torques(q,dq,self.kp,self.kd,x_des, xd_des,F)
             self.robot.send_joint_command(tau)
     
             self.t += 1
